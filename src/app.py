9124858fef6072890a6f6580baad6dd4755f6026
Draken <Starlingliriano@gmail.com>
2026-03-09 04:04:25 -0400

    estos cambios no afectan nada

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from src.messageMenu import Menu
 from src.resourceMenu import Resource
-from src.connexion import Connexion
+from src.connexion import ConnexionSql
 from src.config import verify_token
 import requests
 
