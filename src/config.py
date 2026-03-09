ed9f3e9f5bcf79cf2a28e32e13a06e6fa5b76245
Draken <Starlingliriano@gmail.com>
2026-03-08 20:03:34 -0400

    Agregué configuración con os.getenv y conexión a MySQL

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import os 
+
+verify_token = os.getenv("hub.verify_token")
+mySqlDb = os.getenv("MYSQL_DB")
+mySqlHost = os.getenv("MYSQL_HOST")
+mySqlPassword = os.getenv("MYSQL_PASSWORD")
+mySqlUser = os.getenv("MYSQL_USER")
+whatsappToken = os.getenv("WHATSAPP_TOKEN")
