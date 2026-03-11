c996bd6b378bfae1237508b9b747fd21b57ebf31
Starling Liriano <109784391+DrakenVIP@users.noreply.github.com>
2026-03-10 22:41:28 -0400

    se le agrego el self a los metodos de  verificacion connexion.py

--- src/connexion.py
+++ src/connexion.py
@@ -22,12 +22,12 @@ class ConnexionSql:
             result = cur.fetchone()   # consumir aquí
         return result is not None
     
-    def already_processed(idMessage, cnnx):
+    def already_processed(self,idMessage, cnnx):
         cursor = cnnx.cursor()
         cursor.execute("SELECT 1 FROM processed_messages WHERE id_message = %s", (idMessage,))
         return cursor.fetchone() is not None
 
-    def mark_processed(idMessage, cnnx):
+    def mark_processed(self,idMessage, cnnx):
         cursor = cnnx.cursor()
         cursor.execute("INSERT INTO processed_messages (id_message) VALUES (%s)", (idMessage,))
         cnnx.commit()
